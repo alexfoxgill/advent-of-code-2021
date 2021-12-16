eae86cc6de28e87bec87592fbea5aca6bdbba2d9
Alex Gill <alex.gill@depop.com>
2021-12-16 11:33:21 +0000

    Create parser combinators

--- day16/pt2.exs
+++ day16/pt2.exs
@@ -19,50 +19,52 @@ defmodule Input do
   defp pad_zero([a, b, c, d]), do: [a, b, c, d]
   defp pad_zero(rest), do: pad_zero([0 | rest])
 
-  def parse_bin_list(bits) do
+  defp parse_bin_list(bits) do
     Enum.join(bits) |> Integer.parse(2) |> elem(0)
   end
 
+  defp {a, rest} ~> f, do: {f.(a), rest}
+  defp {a, rest} ~>> f, do: f.(a, rest)
+
+  defp {a, rest} <~> f do
+    {b, rest} = f.(rest)
+    {{a, b}, rest}
+  end
+
   defp parse_packet(rest) do
-    {v, rest} = Enum.split(rest, 3)
-    v = parse_bin_list(v)
-    {t, rest} = Enum.split(rest, 3)
+    {{v, t}, rest} =
+      Enum.split(rest, 3)
+      ~> (&parse_bin_list/1)
+      <~> fn rest -> Enum.split(rest, 3) ~> (&parse_bin_list/1) end
 
-    case parse_bin_list(t) do
+    case t do
       4 ->
-        {literal, rest} = parse_literal(rest)
-
-        packet = %{
-          version: v,
-          type: :literal,
-          literal: literal
-        }
-
-        {packet, rest}
+        parse_literal(rest)
+        ~> (&%{
+              version: v,
+              type: :literal,
+              literal: &1
+            })
 
       opcode ->
-        {ops, rest} = parse_operator(rest)
-
-        packet = %{
-          version: v,
-          type: :operator,
-          opcode: opcode,
-          ops: ops
-        }
-
-        {packet, rest}
+        parse_operator(rest)
+        ~> (&%{
+              version: v,
+              type: :operator,
+              opcode: opcode,
+              ops: &1
+            })
     end
   end
 
   defp parse_literal(rest) do
-    {bin_list, rest} = parse_literal_chunk(rest)
-    {parse_bin_list(bin_list), rest}
+    parse_literal_chunk(rest) ~> (&parse_bin_list/1)
   end
 
   defp parse_literal_chunk([1 | rest]) do
-    {part, rest} = Enum.split(rest, 4)
-    {bin_list, rest} = parse_literal_chunk(rest)
-    {part ++ bin_list, rest}
+    Enum.split(rest, 4)
+    <~> (&parse_literal_chunk/1)
+    ~> fn {a, b} -> a ++ b end
   end
 
   defp parse_literal_chunk([0 | rest]) do
@@ -70,32 +72,28 @@ defmodule Input do
   end
 
   defp parse_operator([0 | rest]) do
-    {len_bin, rest} = Enum.split(rest, 15)
-    len = parse_bin_list(len_bin)
-    {sub_packets_bin, rest} = Enum.split(rest, len)
-
-    sub_packets =
-      Stream.unfold(sub_packets_bin, fn
-        [] -> nil
-        bin -> parse_packet(bin)
-      end)
-      |> Enum.to_list()
-
-    {sub_packets, rest}
+    Enum.split(rest, 15)
+    ~> (&parse_bin_list/1)
+    ~>> (&Enum.split(&2, &1))
+    ~> (&Stream.unfold(&1, fn
+          [] -> nil
+          bin -> parse_packet(bin)
+        end))
+    ~> (&Enum.to_list/1)
   end
 
   defp parse_operator([1 | rest]) do
-    {num_packets_bin, rest} = Enum.split(rest, 11)
-    num_sub_packets = parse_bin_list(num_packets_bin)
-    parse_sub_packets(num_sub_packets, rest)
+    Enum.split(rest, 11)
+    ~> (&parse_bin_list/1)
+    ~>> (&parse_sub_packets/2)
   end
 
   defp parse_sub_packets(0, rest), do: {[], rest}
 
   defp parse_sub_packets(n, rest) do
-    {first_packet, rest} = parse_packet(rest)
-    {other_packets, rest} = parse_sub_packets(n - 1, rest)
-    {[first_packet | other_packets], rest}
+    parse_packet(rest)
+    <~> (&parse_sub_packets(n - 1, &1))
+    ~> fn {a, b} -> [a | b] end
   end
 end
 
@@ -106,8 +104,7 @@ defmodule Puzzle do
 
   def sum_versions(%{type: :literal, version: v}), do: v
 
-  def eval(%{type: literal, literal: v}), do: v
-
+  def eval(%{literal: lit}), do: lit
   def eval(%{opcode: op, ops: ops}), do: ops |> Enum.map(&eval/1) |> apply_op(op)
 
   def apply_op(xs, 0), do: Enum.sum(xs)
